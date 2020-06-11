c10114e11b3d67c3d7694c5d9b980420d16c86eb
sakana <64359744+sakana-fish@users.noreply.github.com>
2020-06-12 02:50:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -43,6 +43,8 @@ async def cal(ctx2):
       if len(rank) == 6:        
         check = 1
         #print("OK")
+      elif rank == end:
+        break
       else:
         await ctx2.send("try again")
     

239b678d09c2a8cf903d578015d62e91c17a17ca
sakana <64359744+sakana-fish@users.noreply.github.com>
2020-07-13 21:54:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,12 +4,12 @@ import os
 import random
 import asyncio
 from discord.ext import commands
-import gspread
-import json
-from oauth2client.service_account import ServiceAccountCredentials 
+#import gspread
+#import json
+#from oauth2client.service_account import ServiceAccountCredentials 
 
 #https://ja.wikipedia.org/wiki/Unicode%E3%81%AEEmoji%E3%81%AE%E4%B8%80%E8%A6%A7
-
+"""
 sheetkey = os.environ['SHEETKEY']
 path = os.environ['MAIL']
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
@@ -18,7 +18,7 @@ gc = gspread.authorize(credentials)
 wb = gc.open_by_key(sheetkey)
 ws = wb.worksheet("戦績記録")
 ws2 = wb.worksheet("交流戦記録")  
-
+"""
 client = commands.Bot(command_prefix='.')
 @client.event
 async def on_ready():
@@ -34,7 +34,7 @@ async def fish(ctx2, about = "🐟🐟🐟 使い方 🐟🐟🐟"):
   help1 = discord.Embed(title=about,color=0xe74c3c,description=".s,.s2,.s3: 交流戦募集開始※12時間で停止 英語スタンプ→挙手 ×スタンプ→挙手全へ\n.rec: 募集開始(.rec 募集名 人数 制限時間(分))\n※募集開始した人の👋スタンプで募集終了\n.cal: 即時集計。順位は16進数でも入力可、recallで呼び戻し、endで強制終了\n.ran 数字: ランダムに数字出力\n.dev 数字 リスト: 組み分け\n.choose リスト: 選択\n.vote: 匿名アンケート(2択)\n戦績記録機能の詳細は.fish2\n作成者: さかな(@sakana8dx)\nさかなBot導入: https://discord.com/oauth2/authorize?client_id=619351049752543234&permissions=473152&scope=bot")
   await ctx2.send(embed=help1)       
  
-
+"""
 @client.command()
 async def t(ctx):
   def check(m):
@@ -129,7 +129,7 @@ async def t(ctx):
   await ctx.send(f"集計終了 {ctx.author.mention}")
   if rev == 1:
     await ctx.send("同組に得点上位が2組以上いないか、得点上位のボーダーに同点がいないかを確認してください")
-
+"""
 
 @client.command()
 async def ran(ctx,arg):
@@ -1045,7 +1045,7 @@ async def s3(ctx, about = "交流戦募集 {}".format(datetime.date.today()), cn
                 atto = f'20@{cnt0} 25@{cnt5} 26@{cnt6}'
                 atto1 = await ctx.send(atto)
 
-                
+"""                
 @client.command()
 async def fish2(ctx, about = "🐟🐟🐟 戦績記録使い方 🐟🐟🐟"):
   help1 = discord.Embed(title=about,color=0xe74c3c,description=".p 点数: 個人の結果記録,符号＋点数を入力する(負けた試合は負),例:.p 100,.p -77\n.r 点差 チーム名: 交流戦の結果記録,例:.r 40 IsK,.r -50 Lv\n.revise 点数: 個人の結果修正,例:.p -80を消す→.revise -80\n.stats/.teamstats/.history: 戦績\n.vs チーム名: 対象チームとの戦績確認\n.rename/.teamrename: 名前の変更\n.reset/.teamreset: 戦績(statsの内容)リセット\n.teamdelete: 対戦履歴削除\n作成者: さかな(@sakana8dx)\nさかなBot導入: https://discord.com/oauth2/authorize?client_id=619351049752543234&permissions=473152&scope=bot")
@@ -1329,7 +1329,7 @@ async def teamrename(ctx):
   msg = await ctx.send("名前を修正しました")
   await asyncio.sleep(3)
   await msg.delete()
-
+"""
 
 token = os.environ['DISCORD_BOT_TOKEN']
 client.run(token)
